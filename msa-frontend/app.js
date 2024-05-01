24d61b1e93d100e8b05323c94d1369adaa477584
yunsung22 <154107485+yunsung22@users.noreply.github.com>
2024-05-01 20:18:54 +0900

    msa-frontend first commit

---
+++ msa-frontend/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var port = 3000;
+var { engine } = require('express-handlebars');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+// 뷰 디렉토리 설정
+app.set('views', path.join(__dirname, 'views', 'templates'));
+
+// 추가로 views/static 경로를 정적 경로로 설정
+app.use(express.static(path.join(__dirname, 'views', 'static')));
+
+// Handlebars 엔진 설정
+app.engine('handlebars', engine({
+  extname: '.handlebars',
+  partialsDir: path.join(__dirname, 'views', 'templates', 'include')  // 파셜 폴더 지정
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index', {  // index.handlebars
+    layout: false  // 레이아웃을 사용하지 않도록 설정
+  });
+});
+
+app.get('/apply', (req, res) => {
+  res.render('apply/apply', {  // apply.handlebars
+    layout: false  // 기본 레이아웃을 사용하지 않도록 설정
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
